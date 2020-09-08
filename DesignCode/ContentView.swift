e49bf3f7399e5d5c0c8617af2c6c5169b46aaed4
Olivier Van hamme <nicefiction@gmail.com>
2020-09-08 12:29:20 +0200

    Adds assets to the project . Creates a stack of cards .

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -8,11 +8,67 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+     // //////////////////////////
+    //  MARK: COMPUTED PROPERTIES
+    
     var body: some View {
-        Text("Hello, World!")
-    }
-}
+        
+        ZStack {
+            VStack {
+                Spacer()
+            }
+            .frame(width : 300.0 , height : 220.0)
+            .background(Color.blue)
+            .cornerRadius(20.0)
+            .shadow(radius: 20.0)
+            .offset(x : 0.0 , y : -20.0)
+            
+            
+            VStack {
+                HStack {
+                    VStack(alignment : .leading) {
+                        Text("Hello, World!")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.white)
+                        Text("Certificate")
+                            .foregroundColor(Color("accent"))
+                    }
+                    Spacer()
+                    Image("Logo1")
+                }
+                .padding([.horizontal , .top] , 20)
+                
+                Spacer()
+                
+                Image("Card1")
+                    .resizable()
+                    .aspectRatio(contentMode : .fill)
+                    .frame(width : 300.0 ,
+                           height : 110.0 ,
+                           alignment : .top)
+            }
+            .frame(width : 340.0 , height : 220.0)
+            .background(Color.black)
+            .cornerRadius(20.0)
+            .shadow(radius : 20)
+        }
+        
+        
+        
+        
+        
+    } // var body: some View {}
+} // struct ContentView: View {}
+
+
+
+
+ // ///////////////
+//  MARK: PREVIEWS
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
